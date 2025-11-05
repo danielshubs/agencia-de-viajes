3355dca20a15f94c4f1af64ee9205ec47940175b
daniel.sanchez.r <daniel.sanchez.r@opendeusto.es>
2025-11-05 18:53:22 +0100

    Páginas de destinos y reserva creada. Página principal y elementos comunes en todas las páginas mejorados

--- agenciaDjango/appAgenciaDjango/urls.py
+++ agenciaDjango/appAgenciaDjango/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('destinos/', views.destinos, name='destinos'),
+    path('destinos/<str:nombre_destino>', views.destinos, name='destino'),
+    path('reservar/', views.reservar, name='reservar'),
+    path('reservar/<str:nombre_destino>', views.reservar, name='reservar'),
 ]
--- agenciaDjango/appAgenciaDjango/views.py
+++ agenciaDjango/appAgenciaDjango/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
+# Ejemplo de lista de destinos turísticos
+lista_destinos = ['París', 'Nueva York', 'Tokio', 'Londres', 'Roma']
+
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'destinos': lista_destinos})
+
+def destinos(request, nombre_destino: str = None):
+    if nombre_destino:
+        return render(request, 'destino.html', {'destino': nombre_destino})
+    else:
+        return render(request, 'destinos.html', {'destinos': lista_destinos})
+
+def reservar(request, nombre_destino: str = None):
+    if nombre_destino:
+        return render(request, 'reservar.html', {'destino': nombre_destino})
+    else:
+        return render(request, 'reservar.html', {'destinos': lista_destinos})
