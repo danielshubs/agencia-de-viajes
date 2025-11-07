fae428b2aa21eead31b07c7b456a6f6843250afd
bernanrdo <bernardo.llata@opendeusto.es>
2025-11-07 12:22:13 +0100

    Creacion Modelos bd , listado de viajes en el index accediendo a la db  ,creacion nueva url para acceder detalles de cada vaije

--- agenciaDjango/appAgenciaDjango/views.py
+++ agenciaDjango/appAgenciaDjango/views.py
@@ -6,27 +6,20 @@ from .models import Viaje
 # Create your views here.
 def index(request):
 
-    lista_viajes = models.Viaje.objects.select_related('destino').all()
-
-    context = {
-        'lista_viajes': lista_viajes,
-    }
-    return render(request, 'index.html' , context)
+    return render(request, 'index.html')
 
 def detalle_viaje(request, viaje_id):
     viaje = get_object_or_404(Viaje, pk=viaje_id)
     return render(request, 'viaje_details.html', {'viaje': viaje})
 
 
-    return render(request, 'index.html', {'destinos': lista_destinos})
-
-def destinos(request, nombre_destino: str = None):
-    if nombre_destino:
-        return render(request, 'destino.html', {'destino': nombre_destino})
-    else:
-        return render(request, 'destinos.html', {'destinos': lista_destinos})
+def destinos(request):
+        lista_destinos = models.Destino.objects.all()
+        return render(request, 'destino.html', {'destino': lista_destinos})
 
 def reservar(request, nombre_destino: str = None):
+    lista_destinos = models.Destino.objects.all()
+
     if nombre_destino:
         return render(request, 'reservar.html', {'destino': nombre_destino})
     else:
